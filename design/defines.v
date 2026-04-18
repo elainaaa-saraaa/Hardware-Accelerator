fc6249e1a1e84bac850c22bac75c1f4a01ceefe7
elainaaa-saraaa <elainasabu@gmail.com>
2026-04-18 14:46:55 +0530

    Create defines.v

---
+++ design/defines.v
@@ -0,0 +1,14 @@
+`ifndef DEFINES_V
+`define DEFINES_V
+
+// Custom 16-bit ISA Opcodes
+`define OPADD    4'b0000
+`define OPSUB    4'b0001
+`define OPMUL    4'b0010
+`define OPMAC    4'b0011
+`define OPLOAD   4'b0100
+`define OPSTORE  4'b0101
+`define OPSHL    4'b0110
+`define OPSTACC  4'b1000
+
+`endif
